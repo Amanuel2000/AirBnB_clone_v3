d4644dec7992e970dcbb1db2c89b252bbdc601c6
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-16 10:47:29 -0800

    Update DBStorage and FileStorage,

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -70,22 +70,6 @@ class FileStorage:
         self.reload()
 
     def get(self, cls, id):
-        """ retrieves """
-        if cls in classes.values() and id and type(id) == str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
-                    return value
-        return None
-
-    def count(self, cls=None):
-        """ counts """
-        data = self.all(cls)
-        if cls in classes.values():
-            data = self.all(cls)
-        return len(data)
-
-    def get(self, cls, id):
         """To retrieve one object """
         if cls in classes.values() and type(id) == str and id:
             my_obj = self.all(cls)
